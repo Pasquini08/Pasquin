cee8929316e6cfa4c614fed0efe9e3f81e04a8ee
Pasquini08 <143645769+Pasquini08@users.noreply.github.com>
2024-03-04 20:29:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-
+# Python com vito
+print "(https://assets.holopin.io/eyJidWNrZXQiOiJob2xvcGluLWFzc2V0cyIsImtleSI6ImFzc2V0cy9jbGN0ZDdjZmYyMjEzMDhtc21iZDUwY2NvIiwiZWRpdHMiOnsicm90YXRlIjpudWxsfX0=)"
+print "(Ola pessualkkk)"
