8b8bef674eba36440cd654c705717841ac846cbb
Pasquini08 <143645769+Pasquini08@users.noreply.github.com>
2024-03-04 20:46:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 # Python com vito
-print "(https://assets.holopin.io/eyJidWNrZXQiOiJob2xvcGluLWFzc2V0cyIsImtleSI6ImFzc2V0cy9jbGN0ZDdjZmYyMjEzMDhtc21iZDUwY2NvIiwiZWRpdHMiOnsicm90YXRlIjpudWxsfX0=)"
-print "(Ola pessualkkk)"
+![CODAQUI MELHOR AQUI](https://assets.holopin.io/eyJidWNrZXQiOiJob2xvcGluLWFzc2V0cyIsImtleSI6ImFzc2V0cy9jbGN0ZDdjZmYyMjEzMDhtc21iZDUwY2NvIiwiZWRpdHMiOnsicm90YXRlIjpudWxsfX0=)
